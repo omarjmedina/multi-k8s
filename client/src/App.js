227a3cd7ade1d066c1ff04d698c602f8bfcf8529
Omar Medina <omar2004@gmail.com>
2021-11-05 19:32:09 -0400

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hola, esta app calcula los indices de fibonacci
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
