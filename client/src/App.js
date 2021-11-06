e7e6ede459df8a27a8d925280223c1498a4c2469
Omar Medina <omar2004@gmail.com>
2021-11-06 02:39:03 -0400

    mod home page

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hola, esta app calcula los indices de fibonacci
+            Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
