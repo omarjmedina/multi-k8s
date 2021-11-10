a9ed91204d3d53675c35e15841356d4195696083
Omar Medina <omar2004@gmail.com>
2021-11-10 01:12:29 -0400

    fix deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ docker build -t omarjmedina/multi-client:latest -t omarjmedina/multi-client:$SHA
 docker build -t omarjmedina/multi-server:latest -t omarjmedina/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t omarjmedina/multi-worker:latest -t omarjmedina/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-doker push omarjmedina/multi-client:latest
-doker push omarjmedina/multi-server:latest
-doker push omarjmedina/multi-worker:latest
+docker push omarjmedina/multi-client:latest
+docker push omarjmedina/multi-server:latest
+docker push omarjmedina/multi-worker:latest
 
-doker push omarjmedina/multi-client:$SHA
-doker push omarjmedina/multi-server:$SHA
-doker push omarjmedina/multi-worker:$SHA
+docker push omarjmedina/multi-client:$SHA
+docker push omarjmedina/multi-server:$SHA
+docker push omarjmedina/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=omarjmedina/multi-server:$SHA
