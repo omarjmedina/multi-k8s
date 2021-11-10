77ef8bc92993fcb1b0c7df2f0fc6ce29e66a1f98
Omar Medina <omar2004@gmail.com>
2021-11-10 11:39:28 -0400

    update header

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator version KUBERNETES!
+            Fib Calculator using KUBERNETES! by Omar Medina
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
